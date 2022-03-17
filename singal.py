c7f51dd35058164fcb688aa5f4c1bc42c5bd93c5
philsong <songbohr@gmail.com>
2022-03-17 19:04:40 +0800

    fix timeout issue.

--- singal.py
+++ singal.py
@@ -8,49 +8,55 @@ from datetime import datetime
 
 configures = {}
 
-async def send_signal(data: dict) -> (dict, bool):
-    print('时间:', datetime.now(), data['side'], data['symbol'])
+index = 0
+
+async def send_signal(signal_data: dict, message: dict) -> (dict, bool):
+    print('时间:', datetime.now(), signal_data['side'], signal_data['symbol'])
     headers = {"Content-Type": "application/json; charset=utf-8"}
 
+    # timeout = aiohttp.ClientTimeout(total=10)
     async with aiohttp.ClientSession() as session:
-      async with session.post(configures['url'], headers=headers, data=json.dumps(data)) as resp:
-        #   print("resp:", resp)
-          print('时间:', datetime.now(), "resp:", resp.status)
-          text = await resp.json()
-          print("resp:", text)
-        
+        # resp = await session.post(configures['url'], headers=headers, data=json.dumps(signal_data))
+        async with session.post(configures['url'], headers=headers, data=json.dumps(signal_data)) as resp:
+            text = await resp.json()
+            print("resp11:",  datetime.now(), resp.status, text)
+
+        async with session.post("http://localhost:5000/register", headers=headers, data=json.dumps(message)) as resp:
+            text = await resp.text()
+            print("resp22:",  datetime.now(), resp.status, text)
     print('****')
 
 async def handle_singal(message_str):
+    global index
+    index+=1
     message = json.loads(message_str)
-    print(message)
     symbol = message["symbol"]
     symbol = symbol.replace('/', '')
-    
+    message["symbol"] = symbol + "PERP"
+    message["index"] = index
+    print(message)
+
     signal_data = configures['data']
     signal_data['side'] = message['action']
     signal_data['symbol'] = symbol + "PERP"
     # print(signal_data)
-    try:
-      await send_signal(signal_data)
-    except Exception as e:
-      print('except')
-      print(e)
+    
+    await send_signal(signal_data, message)
+
 
 async def reader(channel: aioredis.client.PubSub):
     while True:
         try:
-            async with async_timeout.timeout(1):
+            async with async_timeout.timeout(10):
                 message = await channel.get_message(ignore_subscribe_messages=True)
-                if message is not None:
-                    if 'symbol' in message["data"]:
-                        await handle_singal(message["data"])
-                    else:
-                        print(f"(Reader) Message Received: {message}")
                 await asyncio.sleep(0.01)
+            if message is not None:
+                if 'symbol' in message["data"]:
+                    await handle_singal(message["data"])
+                else:
+                    print(f"(Reader) Message Received: {message}")
         except Exception as e:
             print(e)
-
 
 
 async def main():
